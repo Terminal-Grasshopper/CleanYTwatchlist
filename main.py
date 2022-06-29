ea900c11c3263049d0e73bb17fc6961b827872d8
user <md266@uowmail.edu.au>
2022-06-30 00:53:33 +0400

    basic

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import random
 import pyautogui
 import time
 time.sleep(5)
-x=4000
-numbers=[3900, 3800, 3700, 3600, 3500, 2300, 3300, 3200, 3100, 3000, 2900, 2800, 2700]
-while x>0:
-    pyautogui.leftClick(1862, 306, 0.4)
-    pyautogui.leftClick(1759, 441, 0.4)
-    x-=1
-    if x in numbers:
-        pyautogui.leftClick(96, 62, 0.3)
+
+while True:
+    pyautogui.leftClick(1845, 285, 0.5)
+    pyautogui.leftClick(1700, 417, 0.5)
+    if random.randint(0, 100) < 3:
+        pyautogui.leftClick(82, 44, 0.3)
         time.sleep(5)
